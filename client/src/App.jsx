885bd0e2bef5bd697777b3cf9a277453e92cd5eb
Nabajit Kumar <nabajitkumar2002@gmail.com>
2026-09-24 15:01:22 +0530

    Checkpoint 3

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ const apiUrl = (import.meta.env.VITE_API_URL || '').replace(/\/$/, '');
 function getApiUrl(path) {
   const url = new URL(`${apiUrl}${path}`, window.location.origin);
   const previewToken = new URLSearchParams(window.location.search).get('airoShareToken');
-  if (!apiUrl && previewToken) {
+  if (previewToken && url.origin === window.location.origin) {
     url.searchParams.set('airoShareToken', previewToken);
   }
   return url.toString();
